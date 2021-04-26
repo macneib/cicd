800c2e3abdf7c5ced74787ec7ce9f04afde43ebc
macneib <macneib@gmail.com>
2021-04-26 00:07:58 -0600

    added graphql to api service with postgres

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,42 @@
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { AuthModule } from './resolvers/auth/auth.module';
+import { UserModule } from './resolvers/user/user.module';
+import { PostModule } from './resolvers/post/post.module';
+import { AppResolver } from './resolvers/app.resolver';
+import { DateScalar } from './common/scalars/date.scalar';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from './configs/config';
+import { GraphqlConfig } from './configs/config.interface';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const graphqlConfig = configService.get<GraphqlConfig>('graphql');
+        return {
+          installSubscriptionHandlers: true,
+          buildSchemaOptions: {
+            numberScalarMode: 'integer',
+          },
+          sortSchema: graphqlConfig.sortSchema,
+          autoSchemaFile:
+            graphqlConfig.schemaDestination || './src/schema.graphql',
+          debug: graphqlConfig.debug,
+          playground: graphqlConfig.playgroundEnabled,
+          context: ({ req }) => ({ req }),
+        };
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
+    PostModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver, DateScalar],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,42 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import {
+  CorsConfig,
+  NestConfig,
+  SwaggerConfig,
+} from './app/configs/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
-}
 
+  // Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const nestConfig = configService.get<NestConfig>('nest');
+  const corsConfig = configService.get<CorsConfig>('cors');
+  const swaggerConfig = configService.get<SwaggerConfig>('swagger');
+
+  // Swagger Api
+  if (swaggerConfig.enabled) {
+    const options = new DocumentBuilder()
+      .setTitle(swaggerConfig.title || 'Nestjs')
+      .setDescription(swaggerConfig.description || 'The nestjs API description')
+      .setVersion(swaggerConfig.version || '1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup(swaggerConfig.path || 'api', app, document);
+  }
+
+  // Cors
+  if (corsConfig.enabled) {
+    app.enableCors();
+  }
+
+  await app.listen(process.env.PORT || nestConfig.port || 3333);
+}
 bootstrap();
